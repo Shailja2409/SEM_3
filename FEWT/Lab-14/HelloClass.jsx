ab489c6bd0d15348af2eb6a605a07b6e30eee529
Shailja Bhatt <shailjabhatt2409@gmail.com>
2026-08-08 16:36:39 +0530

    Add files via upload

---
+++ FEWT/Lab-14/HelloClass.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HelloClass extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello World with Class</h2>
+      </div>
+    )
+  }
+}
